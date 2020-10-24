3570264b37d65ab33aa2f003004756df6b6e9803
MihaiPE55 <67576278+MihaiPE55@users.noreply.github.com>
2020-10-24 08:27:31 +0300

    Add files via upload

---
+++ changestatus.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix= '<')
+
+@bot.event
+async def on_ready():
+    print("Bot running with:")
+    print("Username: ", bot.user.name)
+    print("User ID: ", bot.user.id)
+    print('Servers connected to:')
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game('Old Status'))
+
+@bot.command()
+async def changeStatus(ctx, status : str):
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(status))
+    await ctx.send(f'New status: {status}')
+
+bot.runt("token")
