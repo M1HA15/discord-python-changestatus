35a32541dd3da7cc56f12d43c1bc6e49cc37de05
MihaiPE55 <67576278+MihaiPE55@users.noreply.github.com>
2020-11-11 12:42:15 +0200

    Update changestatus.py

--- changestatus.py
+++ changestatus.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix= '<')
+bot = commands.Bot(command_prefix= '<custom_prefix>')
 
 @bot.event
 async def on_ready():
