d3b4cc0083633507abbfdd141cb28c87ef8cfd31
Mihai <67576278+MihaiPE55@users.noreply.github.com>
2020-12-17 08:13:30 +0200

    Update changestatus.py

--- changestatus.py
+++ changestatus.py
@@ -15,4 +15,4 @@ async def changeStatus(ctx, status : str):
     await bot.change_presence(status=discord.Status.online, activity=discord.Game(status))
     await ctx.send(f'New status: {status}')
 
-bot.runt("token")
+bot.run("token")
